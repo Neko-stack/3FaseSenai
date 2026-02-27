f84247959a56591d09d53f0be907c79049052505
GABRIEL LESSA COELHO <gabriel_l_coelho@estudante.sesisenai.org.br>
2026-02-26 21:49:21 -0300

    aula back

--- Desenvolvimento de Sistema/Back/atividade/atividade-2.js
+++ Desenvolvimento de Sistema/Back/atividade/atividade-2.js
@@ -1,35 +1,39 @@
 class Queue {
 
-    constructor (){
+    constructor() {
         this.array = new Array()
     }
 
-    enqueue(value){
-        if(value.isEldery){
-            let contador=0
-            while(contador < this.array.length && this.array[contador].isEldery){ contador++
-            }
+    enqueue(value) {
+        if (value.isEldery) {
+            let contador = 0
+            while (contador < this.array.length && this.array[contador].isEldery) contador++
+            
             this.array.splice(contador, 0, value)
-
-        }else{
+        }
+        else {
             this.array.push(value)
         }
-        
-
     }
 
-    dequeue(){
-        if(this.tamanho === 0) return "fila vazia"
-        const removido = 
-        this.arr [this.inicio] = undefined
+    dequeue() {
+    
+    if(this.tamanho === 0) return "fila vazia"
+        const removido = this.array[this.inicio]
+        this.array [this.inicio] = undefined
         this.inicio = this.inicio +  1
-        this.tamanho = this.tamanho - 1
         return removido
+    
     }
+       
+    
+    
 
-
-    mostrarTamanho(){
+    mostrarTamanho() {
         return this.array.length
+    }
+    mostrarFila() {
+        return this.array
     }
 
 }
@@ -39,8 +43,11 @@ const fila = new Queue();
 fila.enqueue({nome:"Ronald", isEldery:false})
 fila.enqueue({nome:"Felipe", isEldery:false})
 fila.enqueue({nome:"Fred", isEldery:false})
+
 fila.enqueue({nome:"Sebastião", isEldery:true})
+fila.enqueue({nome:"Jertrude", isEldery:true})
 fila.enqueue({nome:"Terezinha", isEldery:true})
+fila.dequeue()
 
 
-
+console.table(fila.mostrarFila())
