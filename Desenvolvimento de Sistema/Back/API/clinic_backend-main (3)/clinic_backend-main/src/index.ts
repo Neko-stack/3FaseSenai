37bcee8708a5b2de5a545c960b300439fd8135e8
Neko-stack <gabriel.evil27@gmail.com>
2026-03-12 21:31:48 -0300

    API

---
+++ Desenvolvimento de Sistema/Back/API/clinic_backend-main (3)/clinic_backend-main/src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { prisma } from './prisma/prisma';
+import type { Usuario } from './prisma/generated/prisma/client';
+
+const app = express();
+app.use(express.json())
+const port = 3000;
+
+app.get('/', (req, res) => {
+  console.log(req)
+  res.send("Hello world")
+})
+
+// Endpoints usuario
+app.get('/usuarios', async (req, res) => {
+  const usuarios = await prisma.usuario.findMany();
+  res.json(usuarios);
+})
+
+app.post("/usuarios", async (req, res) => {
+  console.log(req.body)
+  const dadosUsuario = req.body as Usuario
+  const usuarioCriado = await prisma.usuario.create({
+    data: {
+      email: dadosUsuario.email,
+      nome: dadosUsuario.nome || null
+    }
+  })
+  return res.status(201).json(usuarioCriado)
+})
+
+app.listen(port, () => {
+  console.log("Servidor ta de pé :p")
+})
