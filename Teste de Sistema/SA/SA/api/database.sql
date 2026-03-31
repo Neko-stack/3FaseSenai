f3467fb301fd12bd904a2c17386e7e352292b1dc
Neko-stack <gabriel.evil27@gmail.com>
2026-03-30 21:18:22 -0300

    API SA

--- Teste de Sistema/SA/SA/api/database.sql
+++ Teste de Sistema/SA/SA/api/database.sql
@@ -1,14 +1,22 @@
-CREATE DATABASE IF NOT EXISTS kennel_shop;
-USE kennel_shop;
+CREATE DATABASE IF NOT EXISTS teste;
+USE teste;
 
-CREATE TABLE IF NOT EXISTS animals (
+CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    species VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    telefone DECIMAL (20) NOT NULL
 );
 
-INSERT INTO animals (name, species, age, price) VALUES
-('Buddy', 'Golden Retriever', 2, 500.00),
-('Mittens', 'Siamese Cat', 1, 200.00);
+
+CREATE TABLE motos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(50) NOT NULL,
+    modelo VARCHAR(100) NOT NULL,
+    ano INT NOT NULL,
+    cor VARCHAR(30),
+    preco DECIMAL(10, 2) NOT NULL,
+    descricao TEXT,
+    quilometragem INT DEFAULT 0
+);
