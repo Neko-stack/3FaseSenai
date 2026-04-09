141a0d4a0833b85326b56edf78a57e45c84ac6a7
Neko-stack <gabriel.evil27@gmail.com>
2026-04-08 21:03:22 -0300

    atividade corrigida

--- Desenvolvimento de Sistema/Back/autenticação-front/auth_front/src/pages/login.tsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-
-<p className="text-white mt-4">
-  Não tem conta?{" "}
-  <Link to="/" className="underline">
-    Criar conta
-  </Link>
-</p>
--- Desenvolvimento de Sistema/Back/autenticação-front/auth_front/src/pages/register.tsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-
-<p className="text-gray-800 mt-4">
-  Já tem conta?{" "}
-  <Link to="/register" className="underline">
-    Fazer login
-  </Link>
-</p>
